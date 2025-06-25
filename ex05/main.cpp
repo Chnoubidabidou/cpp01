cf108674c3540a57e1638cc6f67727e7f7dd51de
Louis Grisel <lgrisel@c2r8p2.42lehavre.fr>
2025-06-25 13:01:16 +0200

    Fix

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lgrisel <lgrisel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 19:02:49 by lgrisel           #+#    #+#             */
-/*   Updated: 2025/06/12 14:28:23 by lgrisel          ###   ########.fr       */
+/*   Updated: 2025/06/25 13:00:32 by lgrisel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int	main()
 	harl.complain("INFO");
 	harl.complain("WARNING");
 	harl.complain("ERROR");
+	harl.complain("test");
 	return (0);
 }
